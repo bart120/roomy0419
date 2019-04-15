7463fd3a3d8d39fe36ce92e7d38d814d64afbb5b
Vincent <vleclerc@inow.fr>
2019-04-15 17:07:14 +0200

    modification du menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Roomy';
+
+  user: any = null;
+
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
     MatToolbarModule, MatIconModule, MatButtonModule,
     MatCardModule,
-    MatGridListModule
+    MatGridListModule,
+    MatMenuModule
 } from '@angular/material';
 
 @NgModule({
     imports: [
+        BrowserAnimationsModule,
         MatToolbarModule,
         MatIconModule,
         MatButtonModule,
         MatCardModule,
-        MatGridListModule
+        MatGridListModule,
+        MatMenuModule
     ],
     exports: [
+        BrowserAnimationsModule,
         MatToolbarModule,
         MatIconModule,
         MatButtonModule,
         MatCardModule,
-        MatGridListModule
+        MatGridListModule,
+        MatMenuModule
     ]
 })
 export class MaterialModule { }
