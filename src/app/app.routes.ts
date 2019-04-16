0e14c0cd8941382e9e83dc3274b41966f8817761
Vincent <vleclerc@inow.fr>
2019-04-16 10:32:35 +0200

    ajout des redirections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,19 @@ import { Routes } from '@angular/router';
 import { CreateComponent } from './pages/room/create/create.component';
 import { HomeComponent } from './pages/home/home/home.component';
 import { ListComponent } from './pages/room/list/list.component';
+import { NotfoundComponent } from './pages/home/notfound/notfound.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'notfound', component: NotfoundComponent },
     {
         path: 'rooms',
         children: [
             { path: 'create', component: CreateComponent },
             { path: 'list', component: ListComponent }
         ]
-    }
+    },
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: '**', redirectTo: 'notfound' }
+
 ];
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NotfoundComponent],
   imports: [
     CommonModule
   ]
